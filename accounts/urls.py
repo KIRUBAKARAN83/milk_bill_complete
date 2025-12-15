d8f3397eeea44fe3e13627c7985089b60dee8f87
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2025-12-15 22:45:38 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,18 +11,24 @@ urlpatterns = [
     path('customers/<int:customer_id>/edit/', views.edit_customer, name='edit_customer'),
     path('customers/<int:customer_id>/delete/', views.delete_customer, name='delete_customer'),
 
+    path('customers/<int:customer_id>/chart-data/', views.chart_data, name='chart_data'),
+
     path('entry/add/', views.add_entry, name='add_entry'),
     path('entry/<int:entry_id>/edit/', views.edit_entry, name='edit_entry'),
     path('entry/<int:entry_id>/delete/', views.delete_entry, name='delete_entry'),
     path('entry/<int:entry_id>/restore/', views.restore_entry, name='restore_entry'),
-
-    path('customers/<int:customer_id>/chart-data/', views.chart_data, name='chart_data'),
 
     path('customers/<int:customer_id>/bill/', views.bill_pdf, name='bill_pdf'),
     path(
         'customers/<int:customer_id>/bill/<int:year>/<int:month>/',
         views.bill_pdf,
         name='bill_pdf_month'
+    ),
+
+    path(
+        'customers/<int:customer_id>/send-whatsapp/<int:year>/<int:month>/',
+        views.send_bill_whatsapp,
+        name='send_bill_whatsapp'
     ),
 
     path('monthly-summary/', views.monthly_summary, name='monthly_summary'),
