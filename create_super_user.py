da829c1f66b076d6a44082273f0f84697dafc481
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-22 18:55:30 +0530

    Create create_super_user.py

---
+++ create_super_user.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = "Create default superuser if not exists"
+
+    def handle(self, *args, **kwargs):
+        username = "kiruba_karan_123"
+        email = "dhayalankiruba17@gmail.com"
+        password = "kiruba@123"
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("ℹ️ Superuser already exists")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+        )
+        self.stdout.write("✅ Superuser created successfully")
